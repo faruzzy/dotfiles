e878f4802e3926f3d93b0ed93adb653d6a04708a
Roland Pangu <faruzzy@gmail.com>
2021-06-16 09:02:20 -0700

    Initial install.sh commit
    Finally got to do this :)

---
+++ install.sh
@@ -0,0 +1,142 @@
+#!/bin/bash
+
+# Configure OSX
+# Credit:
+# https://github.com/junegunn/dotfiles/blob/master/install
+# https://github.com/gcuisinier/MacConfig
+
+osascript -e 'tell application "System Preferences" to quit'
+sudo -v
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Install Xcode command lines tools cli
+xcode-select --install
+sleep 1
+osascript <<EOD
+  tell application "System Events"
+    tell process "Install Command Line Developer Tools"
+      keystroke return
+      click button "Agree" of window "License Agreement"
+    end tell
+  end tell
+EOD
+
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+
+# Disable Download quarantine
+defaults write com.apple.LaunchServices LSQuarantine -bool NO
+
+# Configure Touchpad
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+# Graphical App install from Home-brew cask
+brew install flux
+brew install --cask bettertouchtool path-finder
+brew install --cask google-chrome firefox
+brew install --cask caffeine
+brew install --cask keka
+brew install --cask whatsapp
+brew install --cask skitch
+brew install --cask kap
+brew install --cask qbserve
+brew install --cask 1password
+brew install --cask google-backup-and-sync
+brew install --cask visual-studio-code
+brew install --cask pdfsam-basic
+brew install --cask slack
+brew install --cask intellij-idea
+brew install --cask colorsnapper
+brew install --cask rectangle
+brew install --cask alacritty
+brew install --cask karabiner-elements
+brew install --cask maccy
+brew install --cask osxfuse
+brew install --cask nightowl
+#Install Java JDK
+brew install --cask	adoptopenjdk8
+brew install --cask	adoptopenjdk11
+brew install jenv maven
+
+brew tap caskroom/fonts
+brew cask install font-fira-code
+
+# Brew package
+brew install mas mkcert nss
+
+# Install zsh
+brew install zsh shellcheck autojump
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/supercrabtree/k ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/k
+
+mkdir -p $HOME/.shellrc/zshrc.d/
+
+# Install App from Mac App Store
+
+# Affinity Photo
+mas install 824183456
+# Affinity Designer
+Mas install 824171161
+
+# Configure Visual Studio Code
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension msjsdiag.debugger-for-chrome
+code --install-extension github.github-vscode-theme
+code --install-extension graphql.vscode-graphql
+code --install-extension wmaurer.vscode-jumpy
+code --install-extension ms-vsliveshare.vsliveshare
+code --install-extension davidanson.vscode-markdownlint
+code --install-extension pkief.material-icon-theme
+code --install-extension christian-kohler.path-intellisense
+code --install-extension esbenp.prettier-vscode
+code --install-extension prisma.prisma
+code --install-extension vscodevim.vim
+
+# Install GraphViz
+brew install libtool
+brew link libtool
+brew install graphviz
+brew link --overwrite graphviz
+
+#Install Frontend Development tools
+brew install node
+brew install yarn --ignore-dependencies
+
+npm install -g @angular/cli yo  generator-jhipster
+npm install -g tldr
+npm install -g typescript
+
+brew install --cask keepingyouawake \
+	mat visualvm mactex \
+	adoptopenjdk8
+
+# dev tools
+brew install \
+	fd ag ripgrep bat cmake cmus \
+	imagemagick diff-so-fancy gnupg \
+	tree vim wget jq python fzf \
+	reattach-to-user-namespace bash bash-completion tmux \
+	translate-shell
+
+# git-prompt
+if [ ! -e ~/.git-prompt.sh ]; then
+  curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
+fi
+
+git config --global user.email "faruzzy@gmail.com"
+git config --global user.name "Roland Pangu"
+git config --global user.signingKey FEF9C627
+git config --global commit.gpgsign true
+
+#git clone https://github.com/faruzzy/dotfiles
+# for loop for adding symbolic link to the HOME folder
+#tmux source-file ~/.tmux.conf
+#./install-vim
