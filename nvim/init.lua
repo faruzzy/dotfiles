eff96d02092583a8666979d7c1d20a579ec73c44
Roland Pangu <ropangu@amazon.com>
2023-02-14 11:37:40 -0700

    Update Telescope to show hidden files

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,4 @@
 --[[
-
 	                                                       ▟▙
 																												 ▝▘
                ██▃▅▇█▆▖  ▗▟████▙▖   ▄████▄     ██▄  ▄██  ██  ▗▟█▆▄▄▆█▙▖
@@ -253,6 +252,12 @@ require('telescope').setup {
       },
     },
   },
+	pickers = {
+		find_files = {
+			hidden = true,
+			find_command = {'rg', '--files', '--hidden', '-g', '!.git' }
+		},
+	},
 }
 
 -- Color highlighter
