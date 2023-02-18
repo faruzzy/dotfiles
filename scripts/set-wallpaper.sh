b3cadffe681b8fd451ca32395017d1639fd19be0
Roland Pangu <faruzzy@gmail.com>
2023-02-18 12:16:22 -0700

    keep track of useful scripts

--- scripts/set-wallpaper.sh
+++ scripts/set-wallpaper.sh
@@ -1,2 +1,5 @@
+# warning
+# there is a known bug with this script that will cause you to lose access to finder and the background
+# When that happens the fix is to delete the desktoppicture database located at: ~/Library/Application Support/Dock/desktoppicture.db
 read -e IMAGE;
 sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '$IMAGE'" && killall Dock
