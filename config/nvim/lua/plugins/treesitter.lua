2e382a7763c65faf21cf2b18fb5a0e27ba7904df
Roland Pangu <faruzzy@gmail.com>
2023-03-17 16:38:27 -0700

    Run TSUpdate when loading treesitter

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,6 @@
 local M = { -- Highlight, edit, and navigate code
   'nvim-treesitter/nvim-treesitter',
+  build = 'TSUpdate',
   dependencies = {
     'nvim-treesitter/nvim-treesitter-textobjects',
   },
