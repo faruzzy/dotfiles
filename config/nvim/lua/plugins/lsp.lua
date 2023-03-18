ea5061eb7918c74a66620e0dcdbfb74e2fdae98a
Roland Pangu <faruzzy@gmail.com>
2023-03-17 23:50:41 -0700

    Temporarily disabling the lsp <C-k> map
    This is conflicting with the <C-k> tmux key map.

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -61,7 +61,7 @@ function M.config()
 
     -- See `:help K` for why this keymap
     nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-    nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+    -- nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
 
     -- Lesser used LSP functionality
     nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
