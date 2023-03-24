d8ce38c63c67262d45f0d01b42c50a8a57d998de
Roland Pangu <ropangu@amazon.com>
2023-03-23 18:09:59 -0700

    Improve completion
    We're now to get completion for the command line in vim.
    We're now getting completion when searching `/`
    Improved how text from bufffer populated in the completion list.
    Associated icons to completion item types (pretty cool!)

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -13,6 +13,54 @@ function M.config()
   local cmp = require 'cmp'
   local luasnip = require 'luasnip'
 
+  -- Didn't really feel like installing another plug for icons
+  local kind_icons = {
+    Text = "",
+    Method = "",
+    Function = "",
+    Constructor = "",
+    Field = "",
+    Variable = "",
+    Class = "ﴯ",
+    Interface = "",
+    Module = "",
+    Property = "ﰠ",
+    Unit = "",
+    Value = "",
+    Enum = "",
+    Keyword = "",
+    Snippet = "",
+    Color = "",
+    File = "",
+    Reference = "",
+    Folder = "",
+    EnumMember = "",
+    Constant = "",
+    Struct = "",
+    Event = "",
+    Operator = "",
+    TypeParameter = ""
+  }
+
+  local has_words_before = function()
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+
+    return col ~= 0 and vim.api.nvim_buf_get_lines(
+      0,
+      line - 1,
+      line,
+      true
+    )[1]:sub(col, col):match("%s") == nil
+  end
+
+  local select_next_item = cmp.mapping(function(fallback)
+    if cmp.visible() then cmp.select_next_item()
+    elseif luasnip.expand_or_jumpable() then luasnip.expand_or_jump()
+    elseif has_words_before() then cmp.complete()
+    else fallback()
+    end
+  end, { "i", "s"})
+
   luasnip.config.setup {}
 
   cmp.setup {
@@ -25,10 +73,12 @@ function M.config()
       ['<C-d>'] = cmp.mapping.scroll_docs(-4),
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       ['<C-Space>'] = cmp.mapping.complete {},
+      ['<C-e>'] = cmp.mapping.close(),
       ['<CR>'] = cmp.mapping.confirm {
-	behavior = cmp.ConfirmBehavior.Replace,
+	-- behavior = cmp.ConfirmBehavior.Replace,
 	select = true,
       },
+      ['<C-j>'] = select_next_item,
       ['<Tab>'] = cmp.mapping(function(fallback)
 	if cmp.visible() then
 	  cmp.select_next_item()
@@ -48,6 +98,7 @@ function M.config()
 	end
       end, { 'i', 's' }),
     },
+    -- Order dictates priority
     sources = {
       { name = 'luasnip' },
       { name = 'nvim_lsp' },
@@ -56,14 +107,40 @@ function M.config()
       { name = 'path' },
       {
 	name = 'buffer',
-	option = {
-	  get_bufnrs = function()
-	    return vim.api.nvim_list_bufs()
-	  end
-	}
+	option = { get_bufnrs = vim.api.nvim_list_bufs },
       }
     },
+    formatting = {
+      format = function(entry, vim_item)
+	vim_item.kind = string.format(
+	  '%s %s',
+	  kind_icons[vim_item.kind], vim_item.kind
+	)
+
+	vim_item.menu = ({
+	  luasnip = '[Snippet]',
+	  buffer = '[Buffer]',
+	  nvim_lsp = '[LSP]',
+	  path = '[Path]',
+	})[entry.source.name]
+
+	return vim_item
+      end
+    }
   }
+
+  cmp.setup.cmdline(':', {
+    sources = {
+      { name = 'path' },
+      { name = 'cmdline' },
+    },
+  })
+
+  cmp.setup.cmdline('/', {
+    sources = {
+      { name = 'buffer' },
+   },
+  })
 end
 
 return M
