164b9282cedf3bca359f8c1262b436270d53053f
Roland Pangu <faruzzy@gmail.com>
2023-12-17 22:28:46 -0700

    Update mapleader related comment

--- nvim/init.lua
+++ nvim/init.lua
@@ -38,9 +38,9 @@ I hope you enjoy your Neovim journey,
 P.S. You can delete this when you're done too. It's your config now :)
 --]]
 
--- Set <space> as the leader key
+-- Set comma ',' as the leader key
 -- See `:help mapleader`
---  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
+-- NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ','
 vim.g.maplocalleader = ','
 
