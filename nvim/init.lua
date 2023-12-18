de96d3168e134fd69d81b5c0848dce4ddb02426c
Roland Pangu <faruzzy@gmail.com>
2023-12-18 00:01:32 -0700

    Add more plugins configuration

--- nvim/init.lua
+++ nvim/init.lua
@@ -60,6 +60,105 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+local autoPairs_config = function()
+  local apair   = require('nvim-autopairs')
+  local rule    = require('nvim-autopairs.rule')
+  local cond    = require('nvim-autopairs.conds')
+  local end_lua = require('nvim-autopairs.rules.endwise-lua')
+
+  apair.setup({
+    check_ts = true,
+    fastwrap = {
+      map = '<M-e>',
+      chars = { '{', '[', '(', '"', "'" },
+      pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], '%s+', ''),
+      offset = 0, -- Offset from pattern match
+      end_key = '$',
+      keys = 'qwertyuiopzxcvbnmasdfghjkl',
+      check_comma = true,
+      highlight = 'Search',
+      highlight_grey='Comment'
+    },
+    ts_config = {
+      -- lua = {'string'},
+      javascript = {'template_string'},
+    },
+    disable_filetype = {
+      'TelescopePrompt',
+      'vim'
+    }
+  })
+
+  -- add spaces between parentheses.
+  local add_space_paren = {
+    rule(' ', ' ')
+      :with_pair(function (opts)
+        local pair = opts.line:sub(opts.col - 1, opts.col)
+        return vim.tbl_contains({ '()', '[]', '{}' }, pair)
+      end),
+    rule('( ', ' )')
+      :with_pair(function() return false end)
+      :with_move(function(opts)
+        return opts.prev_char:match('.%)') ~= nil
+      end)
+      :use_key(')'),
+    rule('{ ', ' }')
+      :with_pair(function() return false end)
+      :with_move(function(opts)
+        return opts.prev_char:match('.%}') ~= nil
+      end)
+      :use_key('}'),
+    rule('[ ', ' ]')
+      :with_pair(function() return false end)
+      :with_move(function(opts)
+        return opts.prev_char:match('.%]') ~= nil
+      end)
+      :use_key(']'),
+  }
+
+  -- add space around =
+  local add_space_equal = {
+    rule('=', '', { 'typescript', 'javascript' })
+      :with_pair(cond.not_inside_quote())
+      :with_pair(function(opts)
+        local last_char = opts.line:sub(opts.col - 1, opts.col - 1)
+        if last_char:match('[%w%=%s]') then
+          return true
+        end
+        return false
+      end)
+      :replace_endpair(function(opts)
+        local prev_2char = opts.line:sub(opts.col - 2, opts.col - 1)
+        local next_char = opts.line:sub(opts.col, opts.col)
+        next_char = next_char == ' ' and '' or ' '
+        if prev_2char:match('%w$') then
+          return '<bs> =' .. next_char
+        end
+        if prev_2char:match('%=$') then
+          return next_char
+        end
+        if prev_2char:match('=') then
+          return '<bs><bs>=' .. next_char
+        end
+        return ''
+      end)
+      :set_end_pair_length(0)
+      :with_move(cond.none())
+      :with_del(cond.none())
+  }
+
+  -- add arrow key in javascript
+  local add_arrow_js = {
+    rule('%(.*%)%s*%=>$', ' {  }', { 'typescript', 'typescriptreact', 'javascript' })
+      :use_regex(true)
+      :set_end_pair_length(2),
+  }
+
+  apair.add_rules(end_lua)
+  apair.add_rules(add_space_paren)
+  apair.add_rules(add_space_equal)
+  -- apair.add_rules(add_arrow_js)
+end
 -- [[ Configure plugins ]]
 -- NOTE: Here is where you install your plugins.
 --  You can configure plugins using the `config` key.
@@ -68,6 +167,11 @@ vim.opt.rtp:prepend(lazypath)
 --    as they will be available in your neovim runtime.
 require('lazy').setup({
   -- NOTE: First, some plugins that don't require any configuration
+  'mattn/emmet-vim',
+  'windwp/nvim-ts-autotag', -- Use treesitter to autoclose and autorename html tags
+  'Issafalcon/lsp-overloads.nvim',
+  'farmergreg/vim-lastplace', -- Intelligently reopen files at your last edit position.
+  'norcalli/nvim-colorizer.lua',
 
   -- tmux integration
   'tmux-plugins/vim-tmux-focus-events',
@@ -126,6 +230,9 @@ require('lazy').setup({
       -- Snippet Engine & its associated nvim-cmp source
       'L3MON4D3/LuaSnip',
       'saadparwaiz1/cmp_luasnip',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-cmdline',
+      'hrsh7th/cmp-path',
 
       -- Adds LSP completion capabilities
       'hrsh7th/cmp-nvim-lsp',
@@ -370,6 +477,30 @@ require('lazy').setup({
     }
   },
 
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter *.*',
+    config = autoPairs_config
+    -- config = function() require('nvim-autopairs').setup {} end
+  },
+
+  {
+    'max397574/better-escape.nvim',
+    config = function()
+      require("better_escape").setup()
+    end,
+  },
+
+  -- Easy motion like plugin that allows you to jump anywhere in a document
+  {
+    'phaazon/hop.nvim',
+    branch = 'v2', -- optional but strongly recommended
+    config = function()
+      -- you can configure Hop the way you like here; see :h hop-config
+      require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+    end
+  },
+
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
@@ -492,6 +623,9 @@ vim.keymap.set('n', '<leader>pp', ':Git push origin master<cr>')
 
 -- Undotree
 vim.keymap.set('n', '<leader>ut', vim.cmd.UndotreeToggle)
+
+-- This command prompts you to type one key, and will hint that key in the buffer.
+vim.keymap.set('n', '<leader>,', ':HopChar1<cr>')
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
@@ -733,7 +867,7 @@ require('mason-lspconfig').setup()
 local servers = {
   -- clangd = {},
   -- gopls = {},
-  -- pyright = {},
+  pyright = {},
   -- rust_analyzer = {},
   tsserver = {},
   html = { filetypes = { 'html', 'twig', 'hbs'} },
@@ -820,11 +954,29 @@ cmp.setup {
     end, { 'i', 's' }),
   },
   sources = {
-    { name = 'nvim_lsp' },
     { name = 'luasnip' },
+    { name = 'nvim_lsp' },
+    { name = 'emmet' },
     { name = 'path' },
+    {
+      name = 'buffer',
+      option = { get_bufnrs = vim.api.nvim_list_bufs },
+    }
   },
 }
+
+cmp.setup.cmdline(':', {
+  sources = {
+    { name = 'path' },
+    { name = 'cmdline' },
+  },
+})
+
+cmp.setup.cmdline('/', {
+  sources = {
+    { name = 'buffer' }
+  },
+})
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
