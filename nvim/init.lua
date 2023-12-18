f8846ec1f315affc202f4e08ba246e6763cdc237
Roland Pangu <faruzzy@gmail.com>
2023-12-17 22:24:29 -0700

    Add Spaces and Tabs configuration

--- nvim/init.lua
+++ nvim/init.lua
@@ -435,6 +435,15 @@ vim.wo.signcolumn = 'yes'
 vim.o.updatetime = 250
 vim.o.timeoutlen = 300
 
+-- Spaces and Tabs
+vim.o.expandtab = true
+vim.o.shiftround = true
+vim.o.shiftwidth = 2
+vim.o.softtabstop = 2
+vim.o.tabstop = 2
+vim.o.list = true
+vim.o.backspace = 'indent,eol,start'
+
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
 
