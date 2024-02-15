c096d4929cc012d61a72b73d532c9a8fbcb0fd6b
Roland Pangu <faruzzy@gmail.com>
2024-02-15 16:13:31 -0700

    Pointing fidget to legacy tag
    This is done to avoid issue with the upcoming rewrite of the library by
    its author.

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -74,7 +74,7 @@ return {
 
     -- Useful status updates for LSP
     -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-    { 'j-hui/fidget.nvim', opts = {} },
+    { 'j-hui/fidget.nvim', opts = {}, tag = 'legacy' },
 
     -- Additional lua configuration, makes nvim stuff amazing!
     'folke/neodev.nvim',
