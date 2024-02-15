c053669d54d43219480fba668087d780aa558715
Roland Pangu <faruzzy@gmail.com>
2024-02-15 16:41:42 -0700

    Starting to experiment with spectre

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -36,4 +36,21 @@ return {
       'nvim-telescope/telescope-fzf-native.nvim'
     }
   },
+  {
+    'nvim-pack/nvim-spectre',
+    dependencies = {
+      'nvim-lua/plenary.nvim'
+    },
+    config = function ()
+      vim.keymap.set('n', '<leader>st', '<cmd>lua require("spectre").toggle()<CR>', {
+        desc = 'Toggle Spectre'
+      })
+      vim.keymap.set('n', '<leader>srw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
+        desc = "Search and replace current word"
+      })
+      vim.keymap.set('n', '<leader>sp', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
+        desc = "Search on current file"
+      })
+    end
+  },
 }
