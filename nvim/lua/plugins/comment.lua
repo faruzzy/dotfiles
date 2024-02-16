e09f18070f9a48a21470e180aaab51dfe572bef8
Roland Pangu <faruzzy@gmail.com>
2024-02-16 12:55:23 -0700

    chore: [nvim] Make Comment.lua work with react
    This code was mostly shameleslly taken (token?) from jackieaskins@

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -1,11 +1,15 @@
 -- "gc" to comment visual regions/lines
 return {
   'numToStr/Comment.nvim',
-  branch = 'jsx',
-  opts = {
-    pre_hook = function(ctx)
-      return require('Comment.jsx').calculate(ctx)
-    end,
+  dependencies = {
+    { 'JoosepAlviste/nvim-ts-context-commentstring', opts = { autocmd = false } },
   },
-  dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  init = function()
+    vim.g.skip_ts_context_commentstring_module = true
+  end,
+  opts = function()
+    return {
+      pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+    }
+  end,
 }
