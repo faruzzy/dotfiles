1d5b97efb366eca501669b1bae7004aaaa5d6503
Roland Pangu <faruzzy@gmail.com>
2024-02-18 22:15:43 -0700

    Add additional config to catppuccin for aesthetics
    Stolen from jaskins@

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -9,6 +9,65 @@ return {
         light = 'latte',
         dark  = 'macchiato',
       },
+      custom_highlights = function(colors)
+        local editor_highlights = require('catppuccin.groups.editor').get()
+
+        local telescope_selection = vim.tbl_extend('force', editor_highlights.CursorLine, {
+          fg = colors.blue,
+        })
+
+        return {
+          -- Default Highlights
+          CurSearch = vim.tbl_extend('force', editor_highlights.IncSearch, {
+            style = { 'bold' },
+          }),
+          CursorLineNr = {
+            fg = colors.blue,
+            bg = editor_highlights.CursorLine.bg,
+            style = { 'bold' },
+          },
+          Folded = { fg = colors.blue, bg = colors.surface0 },
+          NormalFloat = { bg = colors.base },
+          StatusLine = { fg = colors.text, bg = colors.base },
+          TabLine = { fg = colors.text, bg = colors.surface0 },
+          TabLineSep = { fg = colors.surface0, bg = colors.base },
+          WinBarDiagnosticError = { fg = colors.red, bg = colors.base },
+          WinBarDiagnosticWarn = { fg = colors.yellow, bg = colors.base },
+          WinBarDiagnosticHint = { fg = colors.teal, bg = colors.base },
+          WinBarDiagnosticInfo = { fg = colors.sky, bg = colors.base },
+
+          -- Plugin Highlights
+          -- highlight-undo.nvim
+          HighlightUndo = { link = 'IncSearch' },
+
+          -- nvim-hlslens
+          HlSearchLens = editor_highlights.Search,
+
+          -- nvim-lightbulb
+          LightBulbVirtText = { bg = colors.none },
+
+          -- nvim-tree.lua
+          NvimTreeExecFile = { style = { 'underline', 'bold' } },
+          NvimTreeNormal = { fg = colors.text, bg = colors.base },
+          NvimTreeWinSeparator = { fg = colors.surface1, bg = colors.none },
+
+          -- nvim-treesitter-context
+          TreesitterContext = { fg = colors.text, bg = colors.mantle },
+
+          -- quick-scope
+          QuickScopePrimary = { fg = colors.sapphire, style = { 'underline', 'bold' } },
+          QuickScopeSecondary = { fg = colors.pink, style = { 'underline' } },
+
+          -- telescope.lua
+          TelescopePromptPrefix = { fg = colors.blue },
+          TelescopeResultsDiffAdd = { fg = colors.green },
+          TelescopeResultsDiffChange = { fg = colors.yellow },
+          TelescopeResultsDiffDelete = { fg = colors.red },
+          TelescopeResultsDiffUntracked = { fg = colors.none },
+          TelescopeSelection = telescope_selection,
+          TelescopeSelectionCaret = telescope_selection,
+        }
+      end,
       dim_inactive = {
         enabled     = false,
         shade       = 'dark',
