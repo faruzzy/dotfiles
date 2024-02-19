708d7bd87efd8dba1af4fca3eafbb0ca64828486
Roland Pangu <faruzzy@gmail.com>
2024-02-18 22:18:21 -0700

    chore: add `Search Select Telescope` command

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -58,7 +58,8 @@ function M.config()
   vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
   vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
   vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-  vim.keymap.set('n', '<C-g>', '<cmd>Telescope resume<CR>') -- useful for running the last global grep search
+  vim.keymap.set('n', '<leader>ss', require('telescope.builtin').builtin, { desc = '[S]earch [S]elect Telescope' })
+  vim.keymap.set('n', '<C-g>', require('telescope.builtin').resume, { desc = 'Search Resume' }) -- useful for running the last global grep search
   -- vim.keymap.set('n', '<leader>gs', '<cmd>Telescope git_status<CR>')
 
   -- See `:help telescope.builtin`
