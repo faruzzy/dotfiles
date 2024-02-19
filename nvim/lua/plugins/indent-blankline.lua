0a2cab91970dfdeb89f5ce62ccb62d54102bdee3
Roland Pangu <faruzzy@gmail.com>
2024-02-18 21:52:30 -0700

    feat: add `indent-blankline` initial config
    This was mostly stolen from:
    https://github.com/sindrets/dotfiles/blob/master/.config/nvim/lua/user/plugins/indent-blankline.lua

---
+++ nvim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,36 @@
+return {
+  'lukas-reineke/indent-blankline.nvim',
+  config = function ()
+    -- local hooks = require('ibl.hooks')
+    -- local hi = Config.common.hl.hi
+
+    -- hooks.register(hooks.type.HIGHLIGHT_SETUP, vim.schedule_wrap(function ()
+    --   -- Remove `nocombine`
+    --   hi(
+    --     {
+    --       "@ibl.indent.char.1",
+    --       "@ibl.whitespace.char.1",
+    --       "@ibl.scope.char.1",
+    --       "@ibl.scope.underline.1",
+    --     },
+    --     { style = "NONE" }
+    --   )
+    -- end))
+
+    require('ibl').setup({
+      enabled = true,
+      debounce = 200,
+      indent = {
+        char = "▏",
+        tab_char = "▏",
+        smart_indent_cap = true,
+        highlight = "Whitespace",
+      },
+      scope = {
+        enabled = true,
+        char = "▏",
+        show_start = false,
+      }
+    })
+  end
+}
