ccea0b7dc0bf635d747de38b1420162c30b7277e
Roland Pangu <faruzzy@gmail.com>
2024-02-22 23:11:09 -0700

    feat: add stay-in-place.nvim
    Also updated comment indentation

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -3,7 +3,7 @@ return {
   'mattn/emmet-vim',
   'MaxMEllon/vim-jsx-pretty',
   -- 'Issafalcon/lsp-overloads.nvim',
-  'farmergreg/vim-lastplace', -- Intelligently reopen files at your last edit position.
+  'farmergreg/vim-lastplace',                                   -- Intelligently reopen files at your last edit position.
   'norcalli/nvim-colorizer.lua',
 
   -- tmux integration
@@ -13,21 +13,22 @@ return {
   'wellle/tmux-complete.vim',
 
   -- Git related plugins
-  'tpope/vim-fugitive', -- Git wrapper
-  'tpope/vim-rhubarb', -- Github extension for fugitive
-  'junegunn/gv.vim', -- A git commit browser
+  'tpope/vim-fugitive',                                         -- Git wrapper
+  'tpope/vim-rhubarb',                                          -- Github extension for fugitive
+  'junegunn/gv.vim',                                            -- A git commit browser
   'rhysd/git-messenger.vim', cmd = 'GitMessenger',
 
   -- Misc
-  'Pocco81/auto-save.nvim', -- automatically save your changes so the world doesn't collapse
-  'psliwka/vim-smoothie', -- Smooth scrolling done right
-  -- 'karb94/neoscroll.nvim', -- look into this as a potential replacement for vim-smoothie
-  'wsdjeg/vim-fetch', -- Fetch that line and column, boy!
-  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  'Pocco81/auto-save.nvim',                                     -- automatically save your changes so the world doesn't collapse
+  'psliwka/vim-smoothie',                                       -- Smooth scrolling done right
+  -- 'karb94/neoscroll.nvim',                                   -- look into this as a potential replacement for vim-smoothie
+  'wsdjeg/vim-fetch',                                           -- Fetch that line and column, boy!
+  'tpope/vim-sleuth',                                           -- Detect tabstop and shiftwidth automatically
   'mbbill/undotree',
-  'rcarriga/nvim-notify', -- Notifications library
+  'rcarriga/nvim-notify',                                       -- Notifications library
   'tpope/vim-repeat', keys = { { '.', desc = 'REPEAT' } },
   'AndrewRadev/splitjoin.vim',
-  'RRethy/vim-illuminate', -- automatically hightlight ohter uses of the word under the cursor
-  'moll/vim-bbye', -- allows you to delete buffers without closing your windows or messing up your layout
+  'RRethy/vim-illuminate',                                      -- automatically hightlight ohter uses of the word under the cursor
+  'moll/vim-bbye',                                              -- allows you to delete buffers without closing your windows or messing up your layout
+  { 'gbprod/stay-in-place.nvim', opts = {} },                   -- prevent the cursor from moving when using shift and filter actions
 }
