ba7fa1b47cccc6932eaf4fdfe0ea7423d677bfbb
Roland Pangu <faruzzy@gmail.com>
2024-07-26 01:07:58 -0700

    Update lsp completion configuration
    - Getting signature help
    - We're now showing icons based on the suggestion
    - The suggestion is now in border for better aesthetic
    - <C-Space> triggers completion list

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,8 +1,20 @@
+local border_config = {
+  border = vim.g.border_style,
+  winhighlight = 'FloatBorder:FloatBorder',
+}
+
+local source_menu_map = {
+  buffer = 'buff',
+  lazydev = 'lazy',
+  luasnip = 'snip',
+  nvim_lsp = 'lsp',
+  nvim_lsp_signature_help = 'sig',
+  path = 'path',
+}
+
 return {
   'hrsh7th/nvim-cmp',
   dependencies = {
-    -- Snippet Engine & its associated nvim-cmp source
-    'L3MON4D3/LuaSnip',
     'saadparwaiz1/cmp_luasnip',
     'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-cmdline',
@@ -12,10 +24,10 @@ return {
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-nvim-lsp-signature-help',
     'hrsh7th/cmp-path',
-    'saadparwaiz1/cmp_luasnip',
 
     -- Adds a number of user-friendly snippets
     'rafamadriz/friendly-snippets',
+    'onsails/lspkind.nvim', -- add fancy icons
   },
   config = function()
     local cmp = require 'cmp'
@@ -37,7 +49,10 @@ return {
         ['<C-p>'] = cmp.mapping.select_prev_item(),
         ['<C-d>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete {},
+        ['<C-Space>'] = cmp.mapping(
+          cmp.mapping.complete({ config = { sources = { { name = 'nvim_lsp' } } } }),
+          { 'i', 'c' }
+        ),
         ['<CR>'] = cmp.mapping.confirm {
           behavior = cmp.ConfirmBehavior.Replace,
           select = true,
@@ -62,14 +77,38 @@ return {
         end, { 'i', 's' }),
       },
       sources = {
-        { name = 'luasnip' },
         { name = 'nvim_lsp' },
+        { name = 'nvim_lsp_signature_help' },
+        { name = 'luasnip' },
         { name = 'emmet' },
         { name = 'path' },
         {
           name = 'buffer',
           option = { get_bufnrs = vim.api.nvim_list_bufs },
         }
+      },
+      window = {
+        completion = cmp.config.window.bordered(border_config),
+        documentation = cmp.config.window.bordered(border_config),
+      },
+      formatting = {
+        format = require('lspkind').cmp_format({
+          before = function(entry, vim_item)
+            local source = entry.source.name
+
+            vim_item.menu = source_menu_map[source]
+            if source == 'nvim_lsp' then
+              pcall(function()
+                local utils = require('lsp.utils')
+                vim_item.menu = '[' .. utils.get_server_display_name(entry.source.source.client.name) .. ']'
+              end)
+            end
+
+            vim_item.abbr = string.sub(vim_item.abbr, 1, 50)
+
+            return vim_item
+          end,
+        }),
       },
     }
 
