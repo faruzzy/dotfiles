375198cb856dfd5203ae954083166ffe384b7dca
Roland Pangu <faruzzy@gmail.com>
2024-08-14 23:36:08 -0700

    Updated lazy.vim configuration

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,13 +5,33 @@ require('faruzzy.autocmd')
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.uv.fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  vim.fn.system { 'git', 'clone', '--filter=blob:none','--branch=stable', lazyrepo, lazypath }
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none','--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup('plugins', {
   change_detection = { notify = false },
   checker = { enabled = true, notify = false },
+  ui = {
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = '⌘',
+      config = '🛠',
+      event = '📅',
+      ft = '📂',
+      init = '⚙',
+      keys = '🗝',
+      plugin = '🔌',
+      runtime = '💻',
+      require = '🌙',
+      source = '📄',
+      start = '🚀',
+      task = '📌',
+      lazy = '💤 ',
+    },
+  }
 })
 
 -- Remap for dealing with word wrap
