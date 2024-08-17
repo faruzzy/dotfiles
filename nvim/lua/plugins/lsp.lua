bdf79db202cc8f269d12f749005a7901c4d4657d
Roland Pangu <faruzzy@gmail.com>
2024-08-16 22:56:58 -0700

    Disable conform plugin temporarily
    Lua files were starting to be formatted on save. I need to understand
    what this plugins that before re-enabling it

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -42,7 +42,7 @@ local servers = {
   pyright = {},
   -- rust_analyzer = {},
   tsserver = {},
-  html = { filetypes = { 'html', 'twig', 'hbs'} },
+  html = { filetypes = { 'html', 'twig', 'hbs' } },
 
   lua_ls = {
     Lua = {
@@ -75,9 +75,9 @@ return {
     -- before setting up the servers.
     require('mason').setup()
     local ensure_installed = vim.tbl_keys(servers or {})
-    vim.list_extend(ensure_installed, {
-      'stylua' -- Used to format Lua code
-    })
+    -- vim.list_extend(ensure_installed, {
+    --   'stylua' -- Used to format Lua code
+    -- })
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
     local capabilities = vim.lsp.protocol.make_client_capabilities()
