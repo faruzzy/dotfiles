bf2d6fb6e05bb786f2ead7a37d256e26e04fb2b7
Roland Pangu <faruzzy@gmail.com>
2024-08-18 21:19:49 -0700

    Toggle between relative/absolute numbers

--- nvim/lua/faruzzy/autocmd.lua
+++ nvim/lua/faruzzy/autocmd.lua
@@ -4,7 +4,7 @@ local augroup = require('utils').augroup
 local no_auto_comment = vim.api.nvim_create_augroup('no_auto_comment', { clear = true })
 vim.api.nvim_create_autocmd('FileType', {
   command = 'setlocal formatoptions-=r formatoptions-=o',
-  group = no_auto_comment
+  group = no_auto_comment,
 })
 
 -- [[ Highlight on yank ]]
@@ -59,8 +59,32 @@ augroup('document_highlight_attach', {
         {
           { 'LspDetach' },
           callback = vim.lsp.buf.clear_references,
-        }
+        },
       })
     end,
   },
+})
+
+local numbertoggle = vim.api.nvim_create_augroup('numbertoggle', { clear = true })
+
+-- Toggle between relative/absolute line numbers
+vim.api.nvim_create_autocmd({ 'BufEnter', 'FocusGained', 'InsertLeave', 'CmdlineLeave', 'WinEnter' }, {
+  pattern = '*',
+  group = numbertoggle,
+  callback = function()
+    if vim.o.nu and vim.api.nvim_get_mode().mode ~= 'i' then
+      vim.opt.relativenumber = true
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd({ 'BufLeave', 'FocusLost', 'InsertEnter', 'CmdlineEnter', 'WinLeave' }, {
+  pattern = '*',
+  group = numbertoggle,
+  callback = function()
+    if vim.o.nu then
+      vim.opt.relativenumber = false
+      vim.cmd.redraw()
+    end
+  end,
 })
