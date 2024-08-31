24af473314b94ceeed414d2dfd574617966645cb
Roland Pangu <faruzzy@gmail.com>
2024-08-31 16:10:37 -0700

    feat(neovim): Update paren config to not show ghost text on same line

--- nvim/lua/plugins/paren-hint.lua
+++ nvim/lua/plugins/paren-hint.lua
@@ -6,14 +6,14 @@ return {
   },
   config = function()
     require('paren-hint').setup({
-    -- Include the opening paren in the ghost text
-    include_paren = true,
+      -- Include the opening paren in the ghost text
+      include_paren = true,
 
-    -- Show ghost text when cursor is anywhere on the line that includes the close paren rather just when the cursor is on the close paren
-    anywhere_on_line = false,
+      -- Show ghost text when cursor is anywhere on the line that includes the close paren rather just when the cursor is on the close paren
+      anywhere_on_line = false,
 
-    -- show the ghost text when the opening paren is on the same line as the close paren
-    show_same_line_opening = true,
+      -- show the ghost text when the opening paren is on the same line as the close paren
+      show_same_line_opening = false,
     })
   end,
 }
