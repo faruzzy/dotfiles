c188c5d94f92f5278905aa2fdc54c76834669571
Roland Pangu <faruzzy@gmail.com>
2024-09-07 15:30:30 -0700

    chore(neovim): Refactor alpha.lua

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -2,7 +2,7 @@
 return {
   'goolord/alpha-nvim',
   requires = { 'nvim-tree/nvim-web-devicons' },
-  config = function ()
-      require'alpha'.setup(require'alpha.themes.startify'.config)
-  end
+  config = function()
+    require('alpha').setup(require('alpha.themes.startify').config)
+  end,
 }
