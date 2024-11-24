4527ce7c4cfc9d130663e73442b30f258786443d
Roland Pangu <faruzzy@gmail.com>
2024-11-23 18:49:31 -0700

    chore(neovim): added better description for spectre

--- nvim/lua/plugins/spectre.lua
+++ nvim/lua/plugins/spectre.lua
@@ -1,20 +1,23 @@
+-- Plugin for searching and replacing globally
+-- Press '?' once the search word and replace word have been set
+
 return {
   'nvim-pack/nvim-spectre',
   dependencies = {
-    'nvim-lua/plenary.nvim'
+    'nvim-lua/plenary.nvim',
   },
-  config = function ()
+  config = function()
     vim.keymap.set('n', '<leader>S', '<cmd>lua require("spectre").toggle()<CR>', {
-      desc = 'Toggle Spectre'
+      desc = 'Toggle Spectre',
     })
     vim.keymap.set('n', '<leader>srw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
-      desc = "Search and replace current word"
+      desc = 'Search and replace current word',
     })
     vim.keymap.set('v', '<leader>srw', '<cmd>lua require("spectre").open_visual()<CR>', {
-      desc = "Search and replace current word"
+      desc = 'Search and replace current word',
     })
     vim.keymap.set('n', '<leader>sf', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
-      desc = "Search on current file"
+      desc = 'Search and replace in current file',
     })
-  end
+  end,
 }
