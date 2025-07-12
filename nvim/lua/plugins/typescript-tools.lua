398a98091bc7ad585f3dd7ae7231951e879c90ec
Roland Pangu <faruzzy@gmail.com>
2025-07-12 15:42:13 -0700

    fix(nvim): Improve typescript-tools plugin

--- nvim/lua/plugins/typescript-tools.lua
+++ nvim/lua/plugins/typescript-tools.lua
@@ -1,7 +1,65 @@
+---@type LazySpec
+
 return {
   'pmizio/typescript-tools.nvim',
-  requires = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+  enabled = true,
+  lazy = false,
+  dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
   config = function()
-    require('typescript-tools').setup({})
+    require('typescript-tools').setup({
+      capabilities = require('lsp.capabilities')(),
+      settings = {
+        complete_function_calls = false,
+        include_completions_with_insert_text = true,
+        -- Disable jsx_close_tag in favor of my own handling, default handling breaks emmet completion
+        jsx_close_tag = { enable = false },
+        tsserver_file_preferences = {
+          includeInlayEnumMemberValueHints = true,
+          includeInlayFunctionLikeReturnTypeHints = false,
+          includeInlayFunctionParameterTypeHints = true,
+          includeInlayParameterNameHints = 'all',
+          includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+          includeInlayPropertyDeclarationTypeHints = true,
+          includeInlayVariableTypeHints = false,
+          includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+        },
+        tsserver_plugins = { 'typescript-svelte-plugin' },
+      },
+      handlers = {
+        ['textDocument/publishDiagnostics'] = require('typescript-tools.api').filter_diagnostics({
+          80001, -- File is a CommonJS module; it may be converted to an ES module.
+        }),
+      },
+      on_attach = function(client, bufnr)
+        local bsk = require('utils').buffer_map(bufnr)
+
+        bsk('n', '<leader>oi', '<cmd>TSToolsOrganizeImports<CR>', { desc = 'Organize TypeScript imports' })
+        bsk('n', '<leader>ia', '<cmd>TSToolsAddMissingImports<CR>', { desc = 'Add missing TypeScript imports' })
+        bsk('n', '<leader>ri', '<cmd>TSToolsRemoveUnusedImports<CR>', { desc = 'Remove unused TypeScript imports' })
+
+        if
+            vim.tbl_contains({
+              'javascript',
+              'javascriptreact',
+              'typescriptreact',
+            }, vim.bo.filetype)
+        then
+          bsk('i', '>', function()
+            local success, result = pcall(function()
+              require('typescript-tools.api').jsx_close_tag(
+                bufnr,
+                vim.lsp.util.make_position_params(0, client.offset_encoding or 'utf-16'),
+                vim.schedule,
+                nil
+              )
+            end)
+            if not success and debug then
+              print('jsx_close_tag failed: ' .. tostring(result))
+            end
+            return '>'
+          end, { expr = true, desc = 'Auto-close JSX/TSX tags' })
+        end
+      end,
+    })
   end,
 }
