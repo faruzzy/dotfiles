a8e15a56ea4ea6dcbd397e7567bb168d6ba92247
Roland Pangu <faruzzy@gmail.com>
2025-07-21 00:08:10 -0700

    fix(nvim): prevent neotree to be dimmed by tint.lua

--- nvim/lua/plugins/tint.lua
+++ nvim/lua/plugins/tint.lua
@@ -20,7 +20,7 @@ return {
       window_ignore_function = function(winid)
         local bufid = vim.api.nvim_win_get_buf(winid)
         local filetype = vim.api.nvim_get_option_value('filetype', { buf = bufid })
-        return vim.startswith(filetype, 'Telescope')
+        return vim.startswith(filetype, 'Telescope') or filetype == 'neo-tree'
       end,
     }
   end,
