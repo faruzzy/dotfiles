aa770c8e5698dcd2122c3f6b17d181564058c41d
Roland Pangu <faruzzy@gmail.com>
2025-09-30 23:17:09 -0700

    feat(nvim): enhance diffview.nvim configuration

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -7,11 +7,40 @@ return {
     {
       '<leader>dv',
       function()
-        if vim.wo.diff or vim.bo.filetype == 'DiffviewFiles' then
+        local lib = require('diffview.lib')
+        local view = lib.get_current_view()
+        if view then
           vim.cmd.DiffviewClose()
         else
           vim.cmd.DiffviewOpen()
         end
+      end,
+      desc = 'Toggle Diffview',
+    },
+    {
+      '<leader>dh',
+      '<cmd>DiffviewFileHistory %<cr>',
+      desc = 'Diffview File History (current file)',
+    },
+    {
+      '<leader>dH',
+      '<cmd>DiffviewFileHistory<cr>',
+      desc = 'Diffview File History (all)',
+    },
+    { '<leader>dc', '<cmd>DiffviewClose<cr>',   desc = 'Close Diffview' },
+    { '<leader>dr', '<cmd>DiffviewRefresh<cr>', desc = 'Refresh Diffview' },
+  },
+  opts = {
+    enhanced_diff_hl = true, -- Use better diff highlighting
+    view = {
+      merge_tool = {
+        layout = 'diff3_mixed', -- Better 3-way merge view
+      },
+    },
+    hooks = {
+      diff_buf_read = function()
+        vim.opt_local.wrap = false
+        vim.opt_local.list = false
       end,
     },
   },
