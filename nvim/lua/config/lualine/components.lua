1b34c36482db0816f464f2a1109ea16e35ebf68e
Roland Pangu <faruzzy@gmail.com>
2025-12-12 23:09:01 -0700

    refactor(lualine): reorganize config and add new statusline components

---
+++ nvim/lua/config/lualine/components.lua
@@ -0,0 +1,153 @@
+-- Custom lualine components
+
+local M = {}
+
+-- Priority list for main language servers
+M.PRIORITY_LSP_SERVERS = {
+  'typescript-tools',
+  'ts_ls',
+  'lua_ls',
+  'pyright',
+  'rust_analyzer',
+  'gopls',
+  'clangd',
+  'svelte',
+  'cssls',
+  'html',
+  'graphql',
+  'jsonls',
+  'yamlls',
+  'vimls',
+}
+
+-- LSP server name with count
+M.active_lsp = {
+  function()
+    local clients = vim.lsp.get_clients({ bufnr = 0 })
+    if next(clients) == nil then
+      return ''
+    end
+
+    -- Find priority server
+    local main_server = nil
+    for _, priority in ipairs(M.PRIORITY_LSP_SERVERS) do
+      for _, client in ipairs(clients) do
+        if client.name == priority then
+          main_server = client.name
+          break
+        end
+      end
+      if main_server then
+        break
+      end
+    end
+
+    -- Count total non-null-ls clients
+    local count = 0
+    for _, client in ipairs(clients) do
+      if client.name ~= 'null-ls' then
+        count = count + 1
+      end
+    end
+
+    if main_server then
+      if count > 1 then
+        return string.format('%s +%d', main_server, count - 1)
+      end
+      return main_server
+    end
+
+    -- Fallback to first non-null-ls client
+    for _, client in ipairs(clients) do
+      if client.name ~= 'null-ls' then
+        return client.name
+      end
+    end
+
+    return clients[1] and clients[1].name or ''
+  end,
+  icon = ' LSP:',
+}
+
+-- Macro recording indicator
+M.macro_recording = {
+  function()
+    local reg = vim.fn.reg_recording()
+    if reg == '' then
+      return ''
+    end
+    return 'recording @' .. reg
+  end,
+  color = { fg = '#ff9e64', gui = 'bold' },
+}
+
+-- Search count
+M.search_result = {
+  function()
+    if vim.v.hlsearch == 0 then
+      return ''
+    end
+    local res = vim.fn.searchcount({ maxcount = 999, timeout = 500 })
+    if res.total == 0 then
+      return ''
+    end
+    return string.format('%d/%d', res.current, res.total)
+  end,
+}
+
+-- Spell check indicator
+M.spell_indicator = {
+  function()
+    return vim.wo.spell and '暈' or ''
+  end,
+  cond = function()
+    return vim.wo.spell
+  end,
+  color = { fg = '#7aa2f7' },
+}
+
+-- Smart filename with path shortening
+M.filename = {
+  'filename',
+  path = 1, -- Relative path
+  shorting_target = 40,
+  symbols = {
+    modified = ' ●',
+    readonly = ' ',
+    unnamed = '[No Name]',
+  },
+}
+
+-- Diff source from gitsigns
+M.diff_source = function()
+  local gitsigns = vim.b.gitsigns_status_dict
+  if gitsigns then
+    return {
+      added = gitsigns.added,
+      modified = gitsigns.changed,
+      removed = gitsigns.removed,
+    }
+  end
+end
+
+-- Filetype names for special buffers
+M.filetype_names = {
+  DiffviewFiles = 'Diff View',
+  NeogitStatus = 'Neogit',
+  NvimTree = 'Nvim Tree',
+  Outline = 'Symbols Outline',
+  aerial = 'Aerial',
+  gitcommit = 'Git Commit',
+  startify = 'Startify',
+  startuptime = 'Startup Time',
+  undotree = 'Undo Tree',
+}
+
+M.ft_upper = {
+  'filetype',
+  fmt = function(name)
+    return M.filetype_names[name] or name
+  end,
+}
+
+return M
--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,29 +1,10 @@
+-- StatusLine configuration for lualine
 -- https://github.com/kuntau/dotfiles/blob/develop/config/nvim/lua/config/plugins/statusline.lua
--- StatusLine, bufferline & tabline configs
--- could be any of powerline, windline, lualine, airline or lightline
 
--- stylua: ignore
-local filetype_names = {
-  DiffviewFiles = 'Diff View',
-  NeogitStatus  = 'Neogit',
-  NvimTree      = 'Nvim Tree',
-  Outline       = 'Symbols Outline',
-  aerial        = 'Aerial',
-  gitcommit     = 'Git Commit',
-  startify      = 'Startify',
-  startuptime   = 'Startup Time',
-  undotree      = 'Undo Tree',
-}
-
-local ft_upper = {
-  'filetype',
-  fmt = function(name)
-    return filetype_names[name] and filetype_names[name] or name
-  end,
-}
+local components = require('config.lualine.components')
 
 local ft_extension = {
-  sections = { lualine_a = { ft_upper } },
+  sections = { lualine_a = { components.ft_upper } },
   filetypes = {
     'DiffviewFiles',
     'NvimTree',
@@ -37,12 +18,19 @@ local ft_extension = {
 }
 
 local help_extension = {
-  sections = { lualine_a = { ft_upper }, lualine_c = { 'filename' }, lualine_z = { 'progress' } },
+  sections = {
+    lualine_a = { components.ft_upper },
+    lualine_c = { 'filename' },
+    lualine_z = { 'progress' },
+  },
   filetypes = { 'help', 'man' },
 }
 
 local neogit_extension = {
-  sections = { lualine_a = { ft_upper }, lualine_b = { 'branch' } },
+  sections = {
+    lualine_a = { components.ft_upper },
+    lualine_b = { 'branch' },
+  },
   filetypes = {
     'gitcommit',
     'NeogitStatus',
@@ -53,270 +41,9 @@ local neogit_extension = {
   },
 }
 
--- Lsp server name .
-local active_lsp = {
-  function()
-    local msg = ''
-    local clients = vim.lsp.get_clients({ bufnr = 0 })
-    if next(clients) == nil then
-      return msg
-    end
-
-    -- Prioritize main language servers over auxiliary ones
-    local priority_servers = {
-      'typescript-tools',
-      'ts_ls',
-      'lua_ls',
-      'pyright',
-      'rust_analyzer',
-      'gopls',
-      'clangd',
-      'svelte',
-      'cssls',
-      'html',
-      'graphql',
-      'jsonls',
-      'yamlls',
-      'vimls',
-    }
-
-    -- First, try to find a priority server
-    for _, priority in ipairs(priority_servers) do
-      for _, client in ipairs(clients) do
-        if client.name == priority then
-          return client.name
-        end
-      end
-    end
-
-    -- Fall back to first non-null-ls client
-    for _, client in ipairs(clients) do
-      if client.name == 'null-ls' then
-        msg = client.name
-      else
-        return client.name
-      end
-    end
-    return msg
-  end,
-  icon = ' LSP:',
-}
-
-local diff_source = function()
-  local gitsigns = vim.b.gitsigns_status_dict
-  if gitsigns then
-    return {
-      added = gitsigns.added,
-      modified = gitsigns.changed,
-      removed = gitsigns.removed,
-    }
-  end
-end
-
--- eviline configs
-local evil_opts = function()
-  -- Color table for highlights
-  -- stylua: ignore
-  local colors = {
-    bg       = '#202328',
-    fg       = '#bbc2cf',
-    yellow   = '#ECBE7B',
-    cyan     = '#008080',
-    darkblue = '#081633',
-    green    = '#98be65',
-    orange   = '#FF8800',
-    violet   = '#a9a1e1',
-    magenta  = '#c678dd',
-    blue     = '#51afef',
-    red      = '#ec5f67',
-  }
-
-  -- Config
-  local config = {
-    options = {
-      -- Disable sections and component separators
-      component_separators = '',
-      section_separators = '',
-      theme = {
-        -- We are going to use lualine_c an lualine_x as left and
-        -- right section. Both are highlighted by c theme .  So we
-        -- are just setting default looks o statusline
-        normal = { c = { fg = colors.fg, bg = colors.bg } },
-        inactive = { c = { fg = colors.fg, bg = colors.bg } },
-      },
-    },
-    sections = {
-      -- these are to remove the defaults
-      lualine_a = {},
-      lualine_b = {},
-      lualine_y = {},
-      lualine_z = {},
-      -- These will be filled later
-      lualine_c = {},
-      lualine_x = {},
-    },
-    inactive_sections = {
-      -- these are to remove the defaults
-      lualine_a = {},
-      lualine_b = {},
-      lualine_y = {},
-      lualine_z = {},
-      lualine_c = {},
-      lualine_x = {},
-    },
-  }
-
-  local conditions = {
-    buffer_not_empty = function()
-      return vim.fn.empty(vim.fn.expand('%:t')) ~= 1
-    end,
-    hide_in_width = function()
-      return vim.fn.winwidth(0) > 80
-    end,
-    check_git_workspace = function()
-      local filepath = vim.fn.expand('%:p:h')
-      local gitdir = vim.fn.finddir('.git', filepath .. ';')
-      return gitdir and #gitdir > 0 and #gitdir < #filepath
-    end,
-  }
-
-  -- Inserts a component in lualine_c at left section
-  local ins_left = function(component)
-    table.insert(config.sections.lualine_c, component)
-  end
-
-  -- Inserts a component in lualine_x ot right section
-  local ins_right = function(component)
-    table.insert(config.sections.lualine_x, component)
-  end
-
-  ins_left({
-    function()
-      return '▊'
-    end,
-    color = { fg = colors.blue },      -- Sets highlighting of component
-    padding = { left = 0, right = 1 }, -- We don't need space before this
-  })
-
-  ins_left({
-    -- mode component
-    function()
-      return ''
-    end,
-    color = function()
-      -- auto change color according to neovims mode
-      local mode_color = {
-        n = colors.red,
-        i = colors.green,
-        v = colors.blue,
-        [''] = colors.blue,
-        V = colors.blue,
-        c = colors.magenta,
-        no = colors.red,
-        s = colors.orange,
-        S = colors.orange,
-        [''] = colors.orange,
-        ic = colors.yellow,
-        R = colors.violet,
-        Rv = colors.violet,
-        cv = colors.red,
-        ce = colors.red,
-        r = colors.cyan,
-        rm = colors.cyan,
-        ['r?'] = colors.cyan,
-        ['!'] = colors.red,
-        t = colors.red,
-      }
-      return { fg = mode_color[vim.fn.mode()] }
-    end,
-    padding = { right = 1 },
-  })
-
-  ins_left({
-    -- filesize component
-    'filesize',
-    cond = conditions.buffer_not_empty,
-  })
-
-  ins_left({
-    'filename',
-    cond = conditions.buffer_not_empty,
-    color = { fg = colors.magenta, gui = 'bold' },
-  })
-
-  ins_left({ 'location' })
-
-  ins_left({ 'progress', color = { fg = colors.fg, gui = 'bold' } })
-
-  ins_left({
-    'diagnostics',
-    sources = { 'nvim_diagnostic' },
-    symbols = { error = ' ', warn = ' ', info = ' ' },
-    diagnostics_color = {
-      color_error = { fg = colors.red },
-      color_warn = { fg = colors.yellow },
-      color_info = { fg = colors.cyan },
-    },
-  })
-
-  -- Insert mid section. You can make any number of sections in neovim :)
-  -- for lualine it's any number greater then 2
-  ins_left({
-    function()
-      return '%='
-    end,
-  })
-
-  ins_left(active_lsp)
-
-  -- Add components to right sections
-  ins_right({
-    'o:encoding',       -- option component same as &encoding in viml
-    fmt = string.upper, -- I'm not sure why it's upper case either ;)
-    cond = conditions.hide_in_width,
-    color = { fg = colors.green, gui = 'bold' },
-  })
-
-  ins_right({
-    'fileformat',
-    fmt = string.upper,
-    icons_enabled = false, -- I think icons are cool but Eviline doesn't have them. sigh
-    color = { fg = colors.green, gui = 'bold' },
-  })
-
-  ins_right({
-    'branch',
-    icon = '',
-    color = { fg = colors.violet, gui = 'bold' },
-  })
-
-  ins_right({
-    'diff',
-    -- Is it me or the symbol for modified us really weird
-    symbols = { added = ' ', modified = '柳 ', removed = ' ' },
-    diff_color = {
-      added = { fg = colors.green },
-      modified = { fg = colors.orange },
-      removed = { fg = colors.red },
-    },
-    cond = conditions.hide_in_width,
-  })
-
-  ins_right({
-    function()
-      return '▊'
-    end,
-    color = { fg = colors.blue },
-    padding = { left = 1 },
-  }) -- code
-
-  return config
-end
-
--- my custom configs
-local norm_opts = {
+local opts = {
   options = {
-    component_separators = { left = '', right = '' },
+    component_separators = { left = '', right = '' },
     disabled_filetypes = { 'startify' },
     always_divide_middle = false,
     globalstatus = true,
@@ -327,34 +54,25 @@ local norm_opts = {
         'tabs',
         mode = 0,
         cond = function()
-          return vim.fn.tabpagenr('$') > 1 and true or false
+          return vim.fn.tabpagenr('$') > 1
         end,
-        separator = { left = '', right = '' },
+        separator = { left = '', right = '' },
       },
       'mode',
     },
     lualine_b = {
-      { 'b:gitsigns_head', icon = '' },
-      { 'diff', source = diff_source },
-      { 'diagnostics', symbols = { error = ' ', warn = ' ', hint = ' ', info = ' ' } },
+      { 'b:gitsigns_head', icon = '' },
+      { 'diff', source = components.diff_source },
+      { 'diagnostics', symbols = { error = ' ', warn = ' ', hint = ' ', info = ' ' } },
     },
     lualine_c = {
-      { 'filename', path = 1 },
+      components.filename,
       { 'aerial' },
     },
     lualine_x = {
-      -- {
-      --   require("noice").api.status.command.get,
-      --   cond = require("noice").api.status.command.has,
-      -- },
-      -- {
-      --   require("noice").api.status.mode.get,
-      --   cond = require("noice").api.status.mode.has,
-      -- },
-      -- {
-      --   require("noice").api.status.search.get,
-      --   cond = require("noice").api.status.search.has,
-      -- },
+      components.macro_recording,
+      components.search_result,
+      components.spell_indicator,
       {
         'fileformat',
         separator = '',
@@ -364,7 +82,6 @@ local norm_opts = {
         colored = false,
         icon_only = true,
         separator = '',
-        -- fmt = string.upper,
       },
       {
         'encoding',
@@ -377,18 +94,15 @@ local norm_opts = {
         require('lazy.status').updates,
         cond = require('lazy.status').has_updates,
       },
-      active_lsp,
+      components.active_lsp,
     },
   },
   inactive_sections = {
     lualine_c = { { 'filename', path = 1 } },
   },
   extensions = {
-    -- 'aerial',
-    -- 'nvim-tree',
     'nvim-dap-ui',
     'quickfix',
-    -- 'symbols-outline',
     ft_extension,
     neogit_extension,
     help_extension,
@@ -399,10 +113,10 @@ return {
   {
     'nvim-lualine/lualine.nvim',
     event = 'BufReadPost',
-    opts = norm_opts,
+    opts = opts,
     config = function(_, opts)
       require('lualine').setup(opts)
     end,
   },
-  { 'edkolev/tmuxline.vim', cmd = 'Tmuxline' }, -- Tmux statusline
+  { 'edkolev/tmuxline.vim', cmd = 'Tmuxline' },
 }
