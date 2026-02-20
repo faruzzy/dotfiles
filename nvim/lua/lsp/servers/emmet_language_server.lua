3483da5ea5b75773384b5af88a93c340427085b4
Roland Pangu <faruzzy@gmail.com>
2026-02-20 15:50:43 -0700

    feat(nvim): enable emmet abbreviations in JSX files

--- nvim/lua/lsp/servers/emmet_language_server.lua
+++ nvim/lua/lsp/servers/emmet_language_server.lua
@@ -1,4 +1,12 @@
 ---@type LspServer
 return {
 	display = "emmet-ls",
+	config = function(config)
+		config.init_options = {
+			includeLanguages = {
+				javascriptreact = 'html',
+			},
+		}
+		return config
+	end,
 }
