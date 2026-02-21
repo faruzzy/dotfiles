e90d4b1ee038e745378ab2fa87dff021bde61e77
Roland Pangu <faruzzy@gmail.com>
2026-02-21 00:03:39 -0700

    chore(nvim): remove hammerspoon/wezterm exclusions from lazydev

--- nvim/lua/plugins/lsp/lazydev.lua
+++ nvim/lua/plugins/lsp/lazydev.lua
@@ -7,21 +7,6 @@ return {
     'folke/lazydev.nvim',
     ft = 'lua',
     opts = {
-      enabled = function(root_dir)
-        if vim.g.lazydev_enabled ~= nil then
-          return vim.g.lazydev_enabled
-        end
-
-        return not vim.tbl_contains(
-          vim.tbl_map(function(path)
-            return vim.fn.expand(path)
-          end, {
-            '~/dotfiles/hammerspoon',
-            '~/dotfiles/wezterm',
-          }),
-          root_dir
-        )
-      end,
       library = {
         'lazy.nvim',
         'luvit-meta/library/uv.lua',
