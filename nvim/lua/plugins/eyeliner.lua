e87464db38a4f4d4ce92a1376c05cdae68b0486d
Roland Pangu <faruzzy@gmail.com>
2026-05-06 14:20:04 -0700

    fix(nvim): only show eyeliner highlights on f/F/t/T keypress
    Enable highlight_on_key so underlines don't persist and get confused
    with diagnostics while typing.

--- nvim/lua/plugins/eyeliner.lua
+++ nvim/lua/plugins/eyeliner.lua
@@ -1,6 +1,11 @@
 return {
   'jinh0/eyeliner.nvim',
   config = function()
+    require('eyeliner').setup({
+      highlight_on_key = true,
+      dim = false,
+    })
+
     local colors = require('colors').get_colors()
 
     require('utils').highlight('EyelinerPrimary', {
