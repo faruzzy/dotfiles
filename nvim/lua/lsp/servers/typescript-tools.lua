3ea162d290177b6068b773d7c43a14486d178b73
Roland Pangu <faruzzy@gmail.com>
2026-05-19 12:54:12 -0700

    chore(nvim): Removed unused typescript-tools file

--- nvim/lua/lsp/servers/typescript-tools.lua
+++
@@ -1,7 +0,0 @@
----@type LspServer
-return {
-  display = 'ts-tools',
-  install = { 'npm', 'install', '-g', 'typescript@5.5.0', 'typescript-svelte-plugin@0.4.0' },   -- Pin versions
-  fallback = { 'yarn', 'global', 'add', 'typescript@5.5.0', 'typescript-svelte-plugin@0.4.0' }, -- Fallback
-  skip_lspconfig = true,
-}
